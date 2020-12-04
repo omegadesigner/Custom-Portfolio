1083cdd32780f2cc64438c66a58299519ab4d886
omegadesigner <omegadesigner@hotmail.com>
2020-12-04 14:54:20 -0500

    deployment

--- client/src/services/auth.js
+++ client/src/services/auth.js
@@ -7,12 +7,7 @@ export const loginUser = async (loginData) => {
   return resp.data.user
 }
 
-// export const registerUser = async (registerData) => {
-//   const resp = await api.post('/users/', { user: registerData })
-//   localStorage.setItem('authToken', resp.data.token);
-//   api.defaults.headers.common.authorization = `Bearer ${resp.data.token}`
-//   return resp.data.user
-// }
+
 
 export const verifyUser = async () => {
   const token = localStorage.getItem('authToken');
